b2e0ad64cff86eefdc3272db9dc3b92cef459e2f
anzhelikakostevkapp2023-byte <anzhelika.kostevka.pp.2023@lpnu.ua>
2026-05-04 17:44:09 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,3 @@
-export function getDaysUntilDeadline(deadline) {
-  const diffTime = new Date(deadline) - new Date();
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-  throw new Error("UTILS IS CALLED");
+export function getDaysUntilDeadline() {
+  return 100;
 }
