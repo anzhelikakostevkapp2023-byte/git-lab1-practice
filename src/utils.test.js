09f0d5b7a6f5744b5e857c4a3b0174a2093e0636
anzhelikakostevkapp2023-byte <anzhelika.kostevka.pp.2023@lpnu.ua>
2026-05-04 18:01:29 +0300

    Update utils.test.js

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,8 +1,8 @@
 import { expect, test, describe } from 'vitest';
 import { getDaysUntilDeadline } from './utils';
 
-describe('getDaysUntilDeadline - failure case', () => {
-  test('має повертати 0, але повертає 100 (навмисна помилка)', () => {
+describe('failure case', () => {
+  test('навмисна помилка', () => {
     expect(getDaysUntilDeadline()).toBe(0);
   });
 });
